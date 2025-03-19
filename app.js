3af78af3a192d2efc4ea0fba11cc60bc53aa9961
Simona <enmmanuela2888@gmail.com>
2025-03-19 10:34:54 +0100

    Reset display engine

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
